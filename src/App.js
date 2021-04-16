b393fca833ca361f50eb083b8ca73079cd48ed7d
Pierrick Croissant <pcroissant@ensc.fr>
2021-04-16 13:49:02 +0200

    feat: add header

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
+import photo from './assets/photo.jpg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+        <img src={photo} className="Header-Picture" alt="Photo d'identité" />
+        <p className="Header-Text">
+          <span className="Header-Text-Name">Pierrick Croissant</span><br/>Élève ingénieur
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+      <body className="App-body">
+        Work in progress
+      </body>
     </div>
   );
 }
