c0451f6a6db7fcc602c22a8b09bb8edb4d56c9d4
Pierrick Croissant <pcroissant@ensc.fr>
2021-04-18 15:49:04 +0200

    feat: add education/experience parts

--- src/App.js
+++ src/App.js
@@ -3,33 +3,113 @@ import logo_github from './assets/logo_github.png';
 import logo_linkedin from './assets/logo_linkedin.png';
 import logo_mail from './assets/logo_mail.png';
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import * as Icons from "@fortawesome/free-solid-svg-icons"
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={photo} className="Header-Picture" alt="Photo d'identité" />
+        <img src={photo} className="Header-Picture" alt="Identité" />
         <p className="Header-Text">
           <span className="Header-Text-Name">Pierrick Croissant</span><br />Élève ingénieur à l'
-          <a className="App-Link" href="https://ensc.bordeaux-inp.fr/fr" target="_blank">
+          <a className="App-Link" href="https://ensc.bordeaux-inp.fr/fr" target="_blank" rel="noreferrer">
             ENSC</a><br />
           <p className="Header-Links">
-            <a href="https://github.com/pcroissant" target="_blank">
-              <img className="Header-Links-Logo" src={logo_github} />
+            <a href="https://github.com/pcroissant" target="_blank" rel="noreferrer">
+              <img className="Header-Links-Logo" src={logo_github} alt="Logo GitHub"/>
             </a>
-            <a href="https://www.linkedin.com/in/pcroissant/" target="_blank">
-              <img className="Header-Links-Logo" src={logo_linkedin} />
+            <a href="https://www.linkedin.com/in/pcroissant/" target="_blank" rel="noreferrer">
+              <img className="Header-Links-Logo" src={logo_linkedin} alt="Logo Linkedin"/>
             </a>
-            <a href="mailto:pcroissant@ensc.fr" target="_blank">
-              <img className="Header-Links-Logo" src={logo_mail} />
+            <a href="mailto:pcroissant@ensc.fr" target="_blank" rel="noreferrer">
+              <img className="Header-Links-Logo" src={logo_mail} alt="Logo Mail"/>
             </a>
           </p>
         </p>
       </header>
       <body className="App-body">
-        <p>WORK IN PROGRESS</p>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur ac leo molestie nisi gravida facilisis sed at odio. Interdum et malesuada fames ac ante ipsum primis in faucibus. Nam nunc justo, fermentum ac vestibulum non, lobortis sed diam. Phasellus efficitur ligula et libero porta blandit eget at massa. In nec sapien placerat, consectetur ante ut, condimentum quam. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Proin ultricies nibh massa, at pulvinar velit molestie et. Ut aliquet ullamcorper sem, ac fringilla enim aliquet eleifend. Pellentesque eu hendrerit magna. Duis ac mi ac felis laoreet porta ac nec orci. Aenean suscipit tortor vel pretium efficitur. Nunc vehicula, quam eget venenatis tristique, mauris diam ultrices ante, ornare tincidunt lacus dui sed sapien. Quisque dapibus lorem eget condimentum porttitor.
+        <h1 className="Body-Title"><FontAwesomeIcon icon={Icons.faGraduationCap} size="1x"/> FORMATIONS</h1>
+        <ul>
+          <div className="Body-Entry">
+            <div className="Body-Entry-Left">
+                <li>
+                  <strong className="Body-Entry-Title">DIPLÔME D’INGÉNIERIE COGNITIQUE (2018 - 2021)</strong><br/>
+                  <a href ="https://ensc.bordeaux-inp.fr/fr" className="Body-Entry-Subtitle" target="_blank" rel="noreferrer">École Nationale Supérieure de Cognitique</a><br/>
+                  <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur ac leo molestie nisi gravida facilisis sed at odio. Interdum et malesuada fames ac ante ipsum  primis in faucibus. Nam nunc justo, fermentum ac vestibulum non, lobortis sed diam. Phasellus efficitur ligula et libero porta blandit eget at massa.
+                  </p>
+                </li>
+            </div>
+            <div className="Body-Entry-Right">
+              <FontAwesomeIcon icon={Icons.faMapMarkerAlt} size="1x"/>
+              <a href="https://goo.gl/maps/GwSunWTqEVz5MQnB7" className="Body-Entry-Link" target="_blank" rel="noreferrer">
+                Bordeaux
+              </a>
+            </div>
+          </div>
+          <div className="Body-Entry">
+            <div className="Body-Entry-Left">
+                <li>
+                  <strong className="Body-Entry-Title">LICENCE DE SCIENCES COGNITIVES (2015 - 2018)</strong><br/>
+                  <a href ="https://www.univ-lyon2.fr/" className="Body-Entry-Subtitle" target="_blank" rel="noreferrer">Université Lumière Lyon II</a><br/>
+                  <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur ac leo molestie nisi gravida facilisis sed at odio. Interdum et malesuada fames ac ante ipsum  primis in faucibus. Nam nunc justo, fermentum ac vestibulum non, lobortis sed diam. Phasellus efficitur ligula et libero porta blandit eget at massa.
+                  </p>
+                </li>
+            </div>
+            <div className="Body-Entry-Right">
+              <FontAwesomeIcon icon={Icons.faMapMarkerAlt} size="1x"/>
+              <a href="https://goo.gl/maps/fqXkK5b74bCyD2z2A" className="Body-Entry-Link" target="_blank" rel="noreferrer">
+                Lyon
+              </a>
+            </div>
+          </div>
+        </ul>
+
+        <h1 className="Body-Title"><FontAwesomeIcon icon={Icons.faBriefcase} size="1x"/> EXPÉRIENCES</h1>
+        <ul>
+          <div className="Body-Entry">
+            <div className="Body-Entry-Left">
+                <li>
+                  <strong className="Body-Entry-Title">INGÉNIEUR MACHINE LEARNING (2020)</strong><br/>
+                  <a href ="https://www.wiidii.com/" className="Body-Entry-Subtitle" target="_blank" rel="noreferrer">Wiidii</a><br/>
+                  <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur ac leo molestie nisi gravida facilisis sed at odio. Interdum et malesuada fames ac ante ipsum  primis in faucibus. Nam nunc justo, fermentum ac vestibulum non, lobortis sed diam. Phasellus efficitur ligula et libero porta blandit eget at massa.
+                  </p>
+                </li>
+            </div>
+            <div className="Body-Entry-Right">
+              <FontAwesomeIcon icon={Icons.faMapMarkerAlt} size="1x"/>
+              <a href="https://g.page/Wiidii?share" className="Body-Entry-Link" target="_blank" rel="noreferrer">
+                Bordeaux
+              </a>
+            </div>
+          </div>
+          <div className="Body-Entry">
+            <div className="Body-Entry-Left">
+                <li>
+                  <strong className="Body-Entry-Title">UX DESIGNER (2019)</strong><br/>
+                  <a href ="https://lucine.fr/" className="Body-Entry-Subtitle" target="_blank" rel="noreferrer">Lucine</a><br/>
+                  <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur ac leo molestie nisi gravida facilisis sed at odio. Interdum et malesuada fames ac ante ipsum  primis in faucibus. Nam nunc justo, fermentum ac vestibulum non, lobortis sed diam. Phasellus efficitur ligula et libero porta blandit eget at massa.
+                  </p>
+                </li>
+            </div>
+            <div className="Body-Entry-Right">
+              <FontAwesomeIcon icon={Icons.faMapMarkerAlt} size="1x"/>
+              <a href="https://goo.gl/maps/xqauJx9atBZ71dvn7" className="Body-Entry-Link" target="_blank" rel="noreferrer">
+                Bordeaux
+              </a>
+            </div>
+          </div>
+        </ul>
+
+
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur ac leo molestie nisi gravida facilisis sed at odio. Interdum et malesuada fames ac ante ipsum primis in faucibus. Nam nunc justo, fermentum ac vestibulum non, lobortis sed diam. Phasellus efficitur ligula et libero porta blandit eget at massa. In nec sapien placerat, consectetur ante ut, condimentum quam. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Proin ultricies nibh massa, at pulvinar velit molestie et. Ut aliquet ullamcorper sem, ac fringilla enim aliquet eleifend. Pellentesque eu hendrerit magna. Duis ac mi ac felis laoreet porta ac nec orci. Aenean suscipit tortor vel pretium efficitur. Nunc vehicula, quam eget venenatis tristique, mauris diam ultrices ante, ornare tincidunt lacus dui sed sapien. Quisque dapibus lorem eget condimentum porttitor.
         <p>Maecenas at neque diam. Proin ut dignissim massa, quis porta neque. Duis non lectus porttitor, iaculis nibh ut, ultricies nunc. Vivamus non mauris venenatis, ultricies eros eu, lacinia nibh. Nunc quis dignissim nisl. Fusce sit amet tellus ultrices, volutpat neque sit amet, euismod quam. Donec mollis dignissim sem, vitae vulputate magna. Vestibulum lacinia neque eros, vitae vulputate turpis rhoncus id. Vivamus ut mauris tincidunt, suscipit lectus eget, rutrum odio. Fusce neque justo, ultrices id dui nec, dictum molestie ligula. Proin augue sapien, dignissim in arcu eu, ornare elementum erat. Nulla viverra leo porttitor vestibulum hendrerit. Donec rutrum efficitur maximus. Cras dapibus, leo vel varius porttitor, nibh velit sollicitudin ipsum, eu faucibus ligula ligula sed lectus. Quisque vestibulum, mi quis faucibus fermentum, mi nibh pellentesque ligula, vel fermentum enim enim ut nunc.</p></p>
       </body>
     </div>
